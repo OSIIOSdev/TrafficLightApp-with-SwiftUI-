c92924b1a670d88347ba68b10495a29086a85c9d
Ilya Osipov <osiios.dev@gmail.com>
2022-10-18 21:52:03 +0300

    Initial Commit

---
+++ TrafficLightApp-with-SwiftUI-/TrafficLightApp_with_SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp_with_SwiftUI_App.swift
+//  TrafficLightApp-with-SwiftUI-
+//
+//  Created by Илья on 18.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp_with_SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
